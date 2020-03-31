883e9dfd41503d6eaabc5c6403b6127fa985e7b7
Stanley Huang <huang.stanley050@gmail.com>
2020-03-31 12:04:07 +1100

    aws config for Amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-southeast-2",
+    BUCKET: "upload-note-app"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-2",
+    URL: "https://scaytfoftl.execute-api.ap-southeast-2.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "ap-southeast-2",
+    USER_POOL_ID: "ap-southeast-2_l6Ji4ZACO",
+    APP_CLIENT_ID: "44mvlejbovs7cjhkapv476m504",
+    IDENTITY_POOL_ID: "ap-southeast-2:3776fb0a-2c4b-467b-98ab-e3f867d10b55"
+  }
+};
