c67d54175a7b1d02ebcf49debe100ac6ac5d5a44
Stanley Huang <huang.stanley050@gmail.com>
2020-03-31 15:14:20 +1100

    loading button while login in

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import { Auth } from "aws-amplify";
+import LoaderButton from "../components/LoaderButton";
 import "./Login.css";
 
 export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
@@ -13,6 +15,7 @@ export default function Login(props) {
 
   async function handleSubmit(event) {
     event.preventDefault();
+    setIsLoading(true);
     try {
       await Auth.signIn(email, password);
       setEmail("");
@@ -21,6 +24,7 @@ export default function Login(props) {
       props.history.push("/");
     } catch (e) {
       alert(e.message);
+      setIsLoading(false);
     }
   }
 
@@ -44,9 +48,15 @@ export default function Login(props) {
             type="password"
           />
         </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+        <LoaderButton
+          block
+          type="submit"
+          bsSize="large"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
           Login
-        </Button>
+        </LoaderButton>
       </form>
     </div>
   );
